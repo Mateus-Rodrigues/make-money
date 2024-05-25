eb4aa5aeeb4834c349235cbb7ca069b8bc8669fd
MATEUS APARECIDO PESSOA RODR <mateus.rodrigues64@etectupa.local>
2024-05-24 22:48:47 -0300

    atualizando styles

--- make-money/src/screens/home/home.js
+++ make-money/src/screens/home/home.js
@@ -1,8 +1,11 @@
 import { Image, Text, View } from "react-native"
 import icons from "../../constants/icons.js";
+import {styles} from "../home/home.style.js";
+
+
 const Home = () => {
     return (<View>
-        <Image source={icons.db} />
+        <Image source={icons.db} styles />
     </View>
     );
 } 
---
+++ make-money/src/screens/home/home.style.js
@@ -0,0 +1,8 @@
+
+
+export const styles = {
+    db: {
+        width: 100,
+        height: 30
+    }
+}
